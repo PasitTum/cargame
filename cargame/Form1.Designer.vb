e94e9d2587ff9f640608f0380a2f4b1ad9c9007e
Pasit Boonpanase <pasit.b@ragnar.co.th>
2023-03-04 08:24:03 +0700

    ssss

--- cargame/Form1.Designer.vb
+++ cargame/Form1.Designer.vb
@@ -25,16 +25,6 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.car = New System.Windows.Forms.PictureBox()
-        Me.race1 = New System.Windows.Forms.PictureBox()
-        Me.race2 = New System.Windows.Forms.PictureBox()
-        Me.race3 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.RoadMover = New System.Windows.Forms.Timer(Me.components)
         Me.RightSide = New System.Windows.Forms.Timer(Me.components)
         Me.LeftSide = New System.Windows.Forms.Timer(Me.components)
@@ -43,21 +33,31 @@ Partial Class Form1
         Me.RacerMover3 = New System.Windows.Forms.Timer(Me.components)
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.car = New System.Windows.Forms.PictureBox()
+        Me.race1 = New System.Windows.Forms.PictureBox()
+        Me.race2 = New System.Windows.Forms.PictureBox()
+        Me.race3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         CType(Me.car, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.race1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.race2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.race3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -81,100 +81,6 @@ Partial Class Form1
         Me.Label2.Size = New System.Drawing.Size(87, 20)
         Me.Label2.TabIndex = 10
         Me.Label2.Text = "SPEED 0"
-        '
-        'car
-        '
-        Me.car.Image = Global.cargame.My.Resources.Resources.car_145008_1280
-        Me.car.Location = New System.Drawing.Point(141, 348)
-        Me.car.Name = "car"
-        Me.car.Size = New System.Drawing.Size(52, 78)
-        Me.car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.car.TabIndex = 14
-        Me.car.TabStop = False
-        '
-        'race1
-        '
-        Me.race1.Image = Global.cargame.My.Resources.Resources._8
-        Me.race1.Location = New System.Drawing.Point(253, 127)
-        Me.race1.Name = "race1"
-        Me.race1.Size = New System.Drawing.Size(52, 78)
-        Me.race1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.race1.TabIndex = 13
-        Me.race1.TabStop = False
-        '
-        'race2
-        '
-        Me.race2.Image = Global.cargame.My.Resources.Resources._7
-        Me.race2.Location = New System.Drawing.Point(141, 45)
-        Me.race2.Name = "race2"
-        Me.race2.Size = New System.Drawing.Size(52, 78)
-        Me.race2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.race2.TabIndex = 12
-        Me.race2.TabStop = False
-        '
-        'race3
-        '
-        Me.race3.Image = Global.cargame.My.Resources.Resources._1
-        Me.race3.Location = New System.Drawing.Point(12, 247)
-        Me.race3.Name = "race3"
-        Me.race3.Size = New System.Drawing.Size(52, 78)
-        Me.race3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.race3.TabIndex = 11
-        Me.race3.TabStop = False
-        '
-        'PictureBox6
-        '
-        Me.PictureBox6.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PictureBox6.Location = New System.Drawing.Point(213, 248)
-        Me.PictureBox6.Name = "PictureBox6"
-        Me.PictureBox6.Size = New System.Drawing.Size(20, 99)
-        Me.PictureBox6.TabIndex = 5
-        Me.PictureBox6.TabStop = False
-        '
-        'PictureBox5
-        '
-        Me.PictureBox5.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PictureBox5.Location = New System.Drawing.Point(97, 248)
-        Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(20, 99)
-        Me.PictureBox5.TabIndex = 4
-        Me.PictureBox5.TabStop = False
-        '
-        'PictureBox4
-        '
-        Me.PictureBox4.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PictureBox4.Location = New System.Drawing.Point(213, 103)
-        Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(20, 99)
-        Me.PictureBox4.TabIndex = 3
-        Me.PictureBox4.TabStop = False
-        '
-        'PictureBox3
-        '
-        Me.PictureBox3.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PictureBox3.Location = New System.Drawing.Point(97, 106)
-        Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(20, 99)
-        Me.PictureBox3.TabIndex = 2
-        Me.PictureBox3.TabStop = False
-        '
-        'PictureBox2
-        '
-        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PictureBox2.Location = New System.Drawing.Point(213, -47)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(20, 99)
-        Me.PictureBox2.TabIndex = 1
-        Me.PictureBox2.TabStop = False
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PictureBox1.Location = New System.Drawing.Point(97, -47)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(20, 99)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
         '
         'RoadMover
         '
@@ -228,14 +134,63 @@ Partial Class Form1
         Me.Button1.UseVisualStyleBackColor = True
         Me.Button1.Visible = False
         '
-        'PictureBox7
+        'Label4
         '
-        Me.PictureBox7.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PictureBox7.Location = New System.Drawing.Point(97, 400)
-        Me.PictureBox7.Name = "PictureBox7"
-        Me.PictureBox7.Size = New System.Drawing.Size(20, 99)
-        Me.PictureBox7.TabIndex = 17
-        Me.PictureBox7.TabStop = False
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(269, 48)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 17)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Label4"
+        '
+        'car
+        '
+        Me.car.Image = Global.cargame.My.Resources.Resources.car_145008_1280
+        Me.car.Location = New System.Drawing.Point(141, 348)
+        Me.car.Name = "car"
+        Me.car.Size = New System.Drawing.Size(52, 78)
+        Me.car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.car.TabIndex = 14
+        Me.car.TabStop = False
+        '
+        'race1
+        '
+        Me.race1.Image = Global.cargame.My.Resources.Resources._8
+        Me.race1.Location = New System.Drawing.Point(253, 127)
+        Me.race1.Name = "race1"
+        Me.race1.Size = New System.Drawing.Size(52, 78)
+        Me.race1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.race1.TabIndex = 13
+        Me.race1.TabStop = False
+        '
+        'race2
+        '
+        Me.race2.Image = Global.cargame.My.Resources.Resources._7
+        Me.race2.Location = New System.Drawing.Point(141, 45)
+        Me.race2.Name = "race2"
+        Me.race2.Size = New System.Drawing.Size(52, 78)
+        Me.race2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.race2.TabIndex = 12
+        Me.race2.TabStop = False
+        '
+        'race3
+        '
+        Me.race3.Image = Global.cargame.My.Resources.Resources._1
+        Me.race3.Location = New System.Drawing.Point(12, 247)
+        Me.race3.Name = "race3"
+        Me.race3.Size = New System.Drawing.Size(52, 78)
+        Me.race3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.race3.TabIndex = 11
+        Me.race3.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox6.Location = New System.Drawing.Point(213, 248)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(20, 99)
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
         '
         'PictureBox9
         '
@@ -246,14 +201,59 @@ Partial Class Form1
         Me.PictureBox9.TabIndex = 18
         Me.PictureBox9.TabStop = False
         '
-        'Label4
+        'PictureBox7
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(269, 48)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(51, 17)
-        Me.Label4.TabIndex = 19
-        Me.Label4.Text = "Label4"
+        Me.PictureBox7.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox7.Location = New System.Drawing.Point(97, 400)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(20, 99)
+        Me.PictureBox7.TabIndex = 17
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox5.Location = New System.Drawing.Point(97, 248)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(20, 99)
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox3.Location = New System.Drawing.Point(97, 106)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(20, 99)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox4.Location = New System.Drawing.Point(213, 103)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(20, 99)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox2.Location = New System.Drawing.Point(213, -47)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(20, 99)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox1.Location = New System.Drawing.Point(97, -47)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(20, 99)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
@@ -289,13 +289,13 @@ Partial Class Form1
         CType(Me.race2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.race3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
